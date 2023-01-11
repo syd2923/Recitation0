d5956706be556f5f28e4a964d1f60bd0bff0277b
dsusydneybrown <skbrown22@students.desu.edu>
2023-01-11 13:50:29 -0500

    Completed

--- src/main/java/Recitation0.java
+++ src/main/java/Recitation0.java
@@ -1,5 +1,4 @@
 public class Recitation0 {
-    public static void main(String [] args) {
-        System.out.println();
+    public static void main(String [] args) { System.out.println("Hello World!");
     }
 }
